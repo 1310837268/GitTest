487d791155ec557f3b655107fef470f608f22723
root <1310837268@qq.com>
2022-04-16 09:59:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @create 2022-04-16 9:56
+ * @function
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
